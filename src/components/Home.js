a2da53709f900ca7120c4f2bf9bd3970c4e6606c
ghdnjs18 <ghdnjs18@naver.com>
2021-12-20 15:29:02 +0900

    home 페이지 구현

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import "./Home.css";
+
+const Home = () => {
+  const [text, setText] = useState("개발자 김호원의 포트폴리오입니다.");
+  const [tmpText, setTmpText] = useState([]);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTmpText(tmpText + text[index]);
+      setIndex(index + 1);
+    }, 100);
+    if (index === text.length) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <div className="Home">
+      <h1>{tmpText}</h1>
+    </div>
+  );
+};
+
+export default Home;
