fc1c03eccc9f190448250e93afd371df357aa110
ghdnjs18 <ghdnjs18@naver.com>
2021-12-21 23:36:15 +0900

    skills 레이아웃 구성

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,86 +4,116 @@ import "./Skills.css";
 const Skills = () => {
   return (
     <div className="skills">
-      <div className="skills-title">SKILLs</div>
+      <div className="skills-title">Skills</div>
       <div className="front-end">
         <h2>Java</h2>
-        <p>
+        <div className="progress-bar">
+          <div className="progress-bar__bar"></div>
+        </div>
+        <div>
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다.
-        </p>
+        </div>
       </div>
       <div className="front-end">
         <h2>Python</h2>
-        <p>
+        <div className="progress-bar">
+          <div className="progress-bar__bar"></div>
+        </div>
+        <div>
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다.
-        </p>
+        </div>
       </div>
       <div className="front-end">
         <h2>C</h2>
-        <p>
+        <div className="progress-bar">
+          <div className="progress-bar__bar"></div>
+        </div>
+        <div>
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다.
-        </p>
+        </div>
       </div>
       <div className="front-end">
         <h2>JavaScript</h2>
-        <p>
+        <div className="progress-bar">
+          <div className="progress-bar__bar"></div>
+        </div>
+        <div>
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다.
-        </p>
+        </div>
       </div>
       <div className="front-end">
         <h2>HTML5</h2>
-        <p>
+        <div className="progress-bar">
+          <div className="progress-bar__bar"></div>
+        </div>
+        <div>
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다.
-        </p>
+        </div>
       </div>
       <div className="front-end">
         <h2>CSS3</h2>
-        <p>
+        <div className="progress-bar">
+          <div className="progress-bar__bar"></div>
+        </div>
+        <div>
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다.
-        </p>
+        </div>
       </div>
       <div className="front-end">
         <h2>React</h2>
-        <p>
+        <div className="progress-bar">
+          <div className="progress-bar__bar"></div>
+        </div>
+        <div>
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다.
-        </p>
+        </div>
       </div>
       <div className="front-end">
         <h2>SpringFrameWork</h2>
-        <p>
+        <div className="progress-bar">
+          <div className="progress-bar__bar"></div>
+        </div>
+        <div>
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다.
-        </p>
+        </div>
       </div>
       <div className="front-end">
         <h2>Github</h2>
-        <p>
+        <div className="progress-bar">
+          <div className="progress-bar__bar"></div>
+        </div>
+        <div>
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다.
-        </p>
+        </div>
       </div>
       <div className="front-end">
         <h2>Github</h2>
-        <p>
+        <div className="progress-bar">
+          <div className="progress-bar__bar"></div>
+        </div>
+        <div>
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
           내용입니다.
-        </p>
+        </div>
       </div>
     </div>
   );
