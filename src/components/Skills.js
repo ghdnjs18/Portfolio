66e27d08539e8069bde25e42c529a96de1ca4c67
ghdnjs18 <ghdnjs18@naver.com>
2021-12-22 16:09:41 +0900

    skills 페이지 css 및 에니메이션 수정

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,16 +6,17 @@ import "./Skills.css";
 const Skills = () => {
   const animatedItem = {
     0: useScrollFadeIn("right", 1, 0, 30),
-    1: useScrollFadeIn("right", 1, 0, 35),
-    2: useScrollFadeIn("left", 1, 0, 35),
-    3: useScrollFadeIn("right", 1, 0, 35),
-    4: useScrollFadeIn("left", 1, 0, 35),
-    5: useScrollFadeIn("right", 1, 0, 35),
-    6: useScrollFadeIn("left", 1, 0, 35),
-    7: useScrollFadeIn("right", 1, 0, 35),
-    8: useScrollFadeIn("left", 1, 0, 35),
-    9: useScrollFadeIn("right", 1, 0, 35),
-    10: useScrollFadeIn("left", 1, 0, 35),
+    1: useScrollFadeIn("right", 1, 0.3, 300),
+    2: useScrollFadeIn("right", 1, 0, 35),
+    3: useScrollFadeIn("left", 1, 0, 35),
+    4: useScrollFadeIn("right", 1, 0, 35),
+    5: useScrollFadeIn("left", 1, 0, 35),
+    6: useScrollFadeIn("right", 1, 0, 35),
+    7: useScrollFadeIn("left", 1, 0, 35),
+    8: useScrollFadeIn("right", 1, 0, 35),
+    9: useScrollFadeIn("left", 1, 0, 35),
+    10: useScrollFadeIn("right", 1, 0, 35),
+    11: useScrollFadeIn("left", 1, 0, 35),
   };
 
   return (
@@ -23,114 +24,119 @@ const Skills = () => {
       <div className="skills-title" {...animatedItem[0]}>
         Skills
       </div>
-      <div className="front-end" {...animatedItem[1]}>
-        <h2>Java</h2>
-        <div className="progress-bar">
-          <div className="progress-bar__bar"></div>
-        </div>
-        <div>
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다.
-        </div>
+      <div className="bar-container">
+        <div className="skill-bar" {...animatedItem[1]}></div>
       </div>
-      <div className="front-end" {...animatedItem[2]}>
-        <h2>Python</h2>
-        <div className="progress-bar">
-          <div className="progress-bar__bar"></div>
+      <div className="skill-contatiner">
+        <div className="skill-content" {...animatedItem[2]}>
+          <h2>Java</h2>
+          <div className="progress-bar">
+            <div className="progress-bar__bar"></div>
+          </div>
+          <div>
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다.
+          </div>
         </div>
-        <div>
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다.
+        <div className="skill-content" {...animatedItem[3]}>
+          <h2>Python</h2>
+          <div className="progress-bar">
+            <div className="progress-bar__bar"></div>
+          </div>
+          <div>
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다.
+          </div>
         </div>
-      </div>
-      <div className="front-end" {...animatedItem[3]}>
-        <h2>C</h2>
-        <div className="progress-bar">
-          <div className="progress-bar__bar"></div>
+        <div className="skill-content" {...animatedItem[4]}>
+          <h2>C</h2>
+          <div className="progress-bar">
+            <div className="progress-bar__bar"></div>
+          </div>
+          <div>
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다.
+          </div>
         </div>
-        <div>
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다.
+        <div className="skill-content" {...animatedItem[5]}>
+          <h2>JavaScript</h2>
+          <div className="progress-bar">
+            <div className="progress-bar__bar"></div>
+          </div>
+          <div>
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다.
+          </div>
         </div>
-      </div>
-      <div className="front-end" {...animatedItem[4]}>
-        <h2>JavaScript</h2>
-        <div className="progress-bar">
-          <div className="progress-bar__bar"></div>
+        <div className="skill-content" {...animatedItem[6]}>
+          <h2>HTML5</h2>
+          <div className="progress-bar">
+            <div className="progress-bar__bar"></div>
+          </div>
+          <div>
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다.
+          </div>
         </div>
-        <div>
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다.
+        <div className="skill-content" {...animatedItem[7]}>
+          <h2>CSS3</h2>
+          <div className="progress-bar">
+            <div className="progress-bar__bar"></div>
+          </div>
+          <div>
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다.
+          </div>
         </div>
-      </div>
-      <div className="front-end" {...animatedItem[5]}>
-        <h2>HTML5</h2>
-        <div className="progress-bar">
-          <div className="progress-bar__bar"></div>
+        <div className="skill-content" {...animatedItem[8]}>
+          <h2>React</h2>
+          <div className="progress-bar">
+            <div className="progress-bar__bar"></div>
+          </div>
+          <div>
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다.
+          </div>
         </div>
-        <div>
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다.
+        <div className="skill-content" {...animatedItem[9]}>
+          <h2>SpringFrameWork</h2>
+          <div className="progress-bar">
+            <div className="progress-bar__bar"></div>
+          </div>
+          <div>
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다.
+          </div>
         </div>
-      </div>
-      <div className="front-end" {...animatedItem[6]}>
-        <h2>CSS3</h2>
-        <div className="progress-bar">
-          <div className="progress-bar__bar"></div>
+        <div className="skill-content" {...animatedItem[10]}>
+          <h2>Github</h2>
+          <div className="progress-bar">
+            <div className="progress-bar__bar"></div>
+          </div>
+          <div>
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다.
+          </div>
         </div>
-        <div>
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다.
-        </div>
-      </div>
-      <div className="front-end" {...animatedItem[7]}>
-        <h2>React</h2>
-        <div className="progress-bar">
-          <div className="progress-bar__bar"></div>
-        </div>
-        <div>
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다.
-        </div>
-      </div>
-      <div className="front-end" {...animatedItem[8]}>
-        <h2>SpringFrameWork</h2>
-        <div className="progress-bar">
-          <div className="progress-bar__bar"></div>
-        </div>
-        <div>
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다.
-        </div>
-      </div>
-      <div className="front-end" {...animatedItem[9]}>
-        <h2>Github</h2>
-        <div className="progress-bar">
-          <div className="progress-bar__bar"></div>
-        </div>
-        <div>
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다.
-        </div>
-      </div>
-      <div className="front-end" {...animatedItem[10]}>
-        <h2>Github</h2>
-        <div className="progress-bar">
-          <div className="progress-bar__bar"></div>
-        </div>
-        <div>
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
-          내용입니다.
+        <div className="skill-content" {...animatedItem[11]}>
+          <h2>Github</h2>
+          <div className="progress-bar">
+            <div className="progress-bar__bar"></div>
+          </div>
+          <div>
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.
+            내용입니다.
+          </div>
         </div>
       </div>
     </div>
