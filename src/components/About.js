b5db9807821c5bd5be9ed84034cf2df100ca30ce
ghdnjs18 <ghdnjs18@naver.com>
2021-12-22 15:41:39 +0900

    about 페이지 에니메이션 적용

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef } from "react";
+import { useScrollFadeIn } from "../lib";
+
 import "./About.css";
 
 const About = () => {
+  const animatedItem = {
+    0: useScrollFadeIn("right", 1, 0, 30),
+    1: useScrollFadeIn("right", 1, 0.3, 300),
+    2: useScrollFadeIn(),
+    3: useScrollFadeIn(),
+    4: useScrollFadeIn(),
+  };
+
   return (
     <div className="about">
-      <div className="about-title">About</div>
-      <div className="bar"></div>
+      <div className="about-title" {...animatedItem[0]}>
+        About
+      </div>
+      <div className="bar-container">
+        <div className="bar" {...animatedItem[1]}></div>
+      </div>
       <div className="about-contents">
-        <div className="about-content">
+        <div className="about-content" {...animatedItem[2]}>
           <div className="about-sub-title">서브 타이틀?</div>
           <p>
             내요옹ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
           </p>
         </div>
-        <div className="about-content">
+        <div className="about-content" {...animatedItem[3]}>
           <div className="about-sub-title">서브 타이틀?</div>
           <p>
             내요옹ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
           </p>
         </div>
-        <div className="about-content">
+        <div className="about-content" {...animatedItem[4]}>
           <div className="about-sub-title">서브 타이틀?</div>
           <p>
             내요옹ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
