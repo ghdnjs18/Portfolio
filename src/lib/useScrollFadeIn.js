a1288c238cde662f7a501235d48d893b3f21d4f9
ghdnjs18 <ghdnjs18@naver.com>
2021-12-22 16:52:40 +0900

    skill css 수정

--- src/lib/useScrollFadeIn.js
+++ src/lib/useScrollFadeIn.js
@@ -25,8 +25,11 @@ const useScrollFadeIn = (
 
   const handleScroll = useCallback(
     ([entry]) => {
+      // ref가 설정된 태그
       const { current } = dom;
-
+      console.log(entry);
+      // 해당 태그의 isIntersecting이 true일 경우, 즉 화면안에 들어왔을 경우
+      // 해당 태그의 스타일을 변경시킨다.
       if (entry.isIntersecting) {
         current.style.transitionProperty = "opacity transform";
         current.style.transitionDuration = `${duration}s`;
@@ -36,6 +39,7 @@ const useScrollFadeIn = (
         current.style.transform = "translate(0, 0)";
       }
     },
+    // useCallback은 state나 props를 사용할 경우 deps로 무조건 넣어줘야한다.
     [duration, delay]
   );
 
@@ -43,6 +47,7 @@ const useScrollFadeIn = (
     let observer;
     const { current } = dom;
 
+    // ref설정된 태그가 있다면,
     if (current) {
       observer = new IntersectionObserver(handleScroll, { threshold: 0.7 });
       observer.observe(current);
