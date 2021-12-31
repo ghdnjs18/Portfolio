33b5499f61ad4fb591637a0da73dde1eb4de418c
ghdnjs18 <ghdnjs18@naver.com>
2021-12-31 14:55:15 +0900

    index 수정

--- src/index.js
+++ src/index.js
@@ -3,32 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import { About, Contact, Home, Projects, Skills } from "./components";
-import "./App.css";
 import reportWebVitals from './reportWebVitals';
 
-function App() {
-  return (
-    <div className="App">
-      {/* home */}
-      <Home />
-      {/* about */}
-      <About />
-      {/* skils */}
-      <Skills />
-      {/* projects */}
-      <Projects />
-      {/* contact */}
-      <Contact />
-    </div>
-  );
-}
+ReactDOM.render(
+<BrowserRouter>
+  <App/>
+</BrowserRouter>,document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
 
-ReactDOM.render(<BrowserRouter>
-  <App/>
-</BrowserRouter>,document.getElementById('app'))
