9e6e43ded200ffd9b8206f4bae3a1de75a801ab8
ghdnjs18 <ghdnjs18@naver.com>
2022-01-07 16:42:03 +0900

    home 화면 수정

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,24 +2,38 @@ import React, { useEffect, useState } from "react";
 import "./Home.css";
 
 const Home = () => {
-  const [text, setText] = useState("포트폴리오입니다.");
+  const [text, setText] = useState("안녕하세요 !");
   const [tmpText, setTmpText] = useState([]);
+  const [text2, setText2] = useState("저의 포토폴리오에 오신건 환영합니다.");
+  const [tmpText2, setTmpText2] = useState([]);
   const [index, setIndex] = useState(0);
+  const [index2, setIndex2] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTmpText(tmpText + text[index]);
-      setIndex(index + 1);
+      if (index < text.length) {
+        setTmpText(tmpText + text[index]);
+        setIndex(index + 1);
+      }
+      if (index >= text.length) {
+        setTmpText2(tmpText2 + text2[index2]);
+        setIndex2(index2 + 1);
+      }
     }, 100);
-    if (index === text.length) {
+    if (index2 === text2.length) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
   });
-
+    
   return (
     <div className="Home">
-      <div>{tmpText}</div>
+      <div className="home-container">
+        <div className="home-contents">
+          <div>{tmpText}</div><br/>
+          <div>{tmpText2}</div>
+        </div>
+      </div>
     </div>
   );
 };
